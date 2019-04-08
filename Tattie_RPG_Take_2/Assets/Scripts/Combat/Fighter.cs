f9d7d14c5d4e437857beee6e905859d79fb7af0f
Chris <stofferdyr@gmail.com>
2019-04-08 19:18:24 +0200

    start lecture 46

--- Tattie_RPG_Take_2/Assets/Scripts/Combat/Fighter.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Combat/Fighter.cs
@@ -14,7 +14,7 @@ namespace RPG.Combat
         [SerializeField] float weaponDamage = 5f;
 
         Health target;
-        float timeSinceLastAttack = 0;
+        float timeSinceLastAttack = Mathf.Infinity;
 
 
         private void Update()
@@ -67,7 +67,7 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
-        public bool CanAttack(CombatTarget combatTarget)
+        public bool CanAttack(GameObject combatTarget)
         {
             //if target is null then we cannot attack
             if (combatTarget == null) { return false; }
@@ -77,7 +77,7 @@ namespace RPG.Combat
         }
 
 
-        public void Attack(CombatTarget combatTarget)
+        public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
--- Tattie_RPG_Take_2/Assets/Scripts/Control/PlayerController.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Control/PlayerController.cs
@@ -26,16 +26,19 @@ namespace RPG.Control
             foreach(RaycastHit hit in hits)
             {
                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+               if(target == null) continue;
 
-               //if we cannot attack
-               if(!GetComponent<Fighter>().CanAttack(target))
+                
+
+                //if we cannot attack
+                if(!GetComponent<Fighter>().CanAttack(target.gameObject))
                {
                     continue;//continue the loop
                 }
 
                if(Input.GetMouseButtonDown(0))
                {
-                    GetComponent<Fighter>().Attack(target);
+                    GetComponent<Fighter>().Attack(target.gameObject);
                }
                return true;
             }
