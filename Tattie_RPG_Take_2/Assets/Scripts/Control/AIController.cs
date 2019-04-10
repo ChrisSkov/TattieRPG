d5ce70f4ac7a165e50a3e8fd904078fd051dd9ef
Chris <stofferdyr@gmail.com>
2019-04-11 00:35:31 +0200

    start lecture 55

--- Tattie_RPG_Take_2/Assets/Scripts/Control/AIController.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Control/AIController.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
 
-namespace  RPG.Control
+namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 3f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float waypointDwellTime = 4f;
 
         Fighter fighter;
         Health health;
@@ -20,12 +24,15 @@ namespace  RPG.Control
 
         Vector3 guardPosition;
         float timeSinceLastSawPlayer = Mathf.Infinity;
-        private void Start() 
+        float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        int currentWaypointIndex = 0;
+
+        private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
             mover = GetComponent<Mover>();
-            
+
             player = GameObject.FindWithTag("Player");
 
             guardPosition = transform.position;
@@ -34,28 +41,67 @@ namespace  RPG.Control
         private void Update()
         {
 
-            if(health.IsDead()) return;
-            if(InAttackRangeOfPlayer()  && fighter.CanAttack(player))
+            if (health.IsDead()) return;
+
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                timeSinceLastSawPlayer = 0;
+
                 AttackBehaviour();
             }
-            else if(timeSinceLastSawPlayer < suspicionTime)
+            else if (timeSinceLastSawPlayer < suspicionTime)
             {
                 SuspicionBehaviour();
             }
             else
             {
-                GuardBehaviour();
+                PatrolBehaviour();
 
             }
 
-            timeSinceLastSawPlayer += Time.deltaTime;
+            UpdateTimers();
         }
 
-        private void GuardBehaviour()
+        private void UpdateTimers()
         {
-            mover.StartMoveAction(guardPosition);
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = guardPosition;
+
+            if (patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            if( timeSinceArrivedAtWaypoint > waypointDwellTime )
+            {
+
+                mover.StartMoveAction(nextPosition);
+            }
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
+        }
+
+        private void CycleWaypoint()
+        {
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
         }
 
         private void SuspicionBehaviour()
@@ -65,6 +111,7 @@ namespace  RPG.Control
 
         private void AttackBehaviour()
         {
+            timeSinceLastSawPlayer = 0;
             fighter.Attack(player);
         }
 
@@ -80,8 +127,8 @@ namespace  RPG.Control
             Gizmos.DrawWireSphere(transform.position, chaseDistance);
 
         }
-        
-            
-        
+
+
+
     }
 }
--- Tattie_RPG_Take_2/Assets/Scripts/Control/PlayerController.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Control/PlayerController.cs
@@ -45,7 +45,7 @@ namespace RPG.Control
                     continue;//continue the loop
                 }
 
-               if(Input.GetMouseButtonDown(0))
+               if(Input.GetMouseButton(0))
                {
                     GetComponent<Fighter>().Attack(target.gameObject);
                }
