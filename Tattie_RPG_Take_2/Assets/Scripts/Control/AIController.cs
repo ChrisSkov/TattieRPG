0778ead3a2687a4a5592231d23fdbed4d888fd4f
Chris <stofferdyr@gmail.com>
2019-04-15 22:11:21 +0200

    star lecture 63

--- Tattie_RPG_Take_2/Assets/Scripts/Control/AIController.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Control/AIController.cs
@@ -16,6 +16,9 @@ namespace RPG.Control
         [SerializeField] float waypointTolerance = 1f;
         [SerializeField] float waypointDwellTime = 4f;
 
+        [Range(0,1)]
+        [SerializeField] float patrolSpeedFraction = 0.2f;
+
         Fighter fighter;
         Health health;
         Mover mover;
@@ -84,7 +87,7 @@ namespace RPG.Control
             if( timeSinceArrivedAtWaypoint > waypointDwellTime )
             {
 
-                mover.StartMoveAction(nextPosition);
+                mover.StartMoveAction(nextPosition, patrolSpeedFraction);
             }
         }
 
