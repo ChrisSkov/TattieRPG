97575ae1819bf8369ee91fec5faa7fec253b0d93
Chris <stofferdyr@gmail.com>
2019-04-21 13:48:12 +0200

    Create player HPbar

--- Tattie_RPG_Take_2/Assets/Scripts/Core/Health.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Core/Health.cs
@@ -1,40 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.Core
 {
     public class Health : MonoBehaviour
     {
+       // public Slider slider;
 
-       [SerializeField] float healthPoints = 100f;
+        
+        [SerializeField] float healthPoints = 100f;
 
-       bool isDead = false;
+        bool isDead = false;
+
+        public float getHealth()
+        {
+            
+            {
+                return healthPoints;
+            }
+        }
 
         public bool IsDead()
         {
             return isDead;
         }
 
-
+   
 
         public void TakeDamage(float damage)
-       {
-            healthPoints = Mathf.Max(healthPoints - damage,0);
-            if(healthPoints == 0)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0)
             {
                 Die();
             }
-            
-       }
 
-       public void Die() {
-           {    if(isDead)return;
+        }
+
+        public void Die()
+        {
+            {
+                if (isDead) return;
 
                 isDead = true;
                 GetComponent<Animator>().SetTrigger("die");
                 GetComponent<ActionScheduler>().CancelCurrentAction();
             }
-       }
+        }
     }
 }
---
+++ Tattie_RPG_Take_2/Assets/SliderHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+using UnityEngine.UI;
+public class SliderHP : MonoBehaviour
+{
+
+    GameObject player;
+    Slider slider;
+    Health playerHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        slider = GetComponent<Slider>();
+        playerHP = player.GetComponent<Health>();
+        
+    }
+
+
+    void setText()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = playerHP.getHealth();
+    }
+}
