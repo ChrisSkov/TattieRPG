f538755671312be8a1b71b17b60b68047e757462
Chris <stofferdyr@gmail.com>
2019-04-22 23:33:48 +0200

    start lecture 74

--- Tattie_RPG_Take_2/Assets/Game/SceneManagement/Portal.cs
+++ Tattie_RPG_Take_2/Assets/Game/SceneManagement/Portal.cs
@@ -8,8 +8,15 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,6 +28,10 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if(sceneToLoad < 0)
+            {
+               yield break;
+            }
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
@@ -42,6 +53,7 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
               if(portal == this) continue;
+              if(portal.destination != destination) continue;
 
                 return portal;
             }
