ae87a2ce08315b174dae9f68c5305c8c71976a8d
Chris <stofferdyr@gmail.com>
2019-04-28 21:55:55 +0200

    Start lecture 85

---
+++ Tattie_RPG_Take_2/Library/Collab/Download/Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SaveableEntity : MonoBehaviour
+    {
+        public string GetUniqueIdentifier()
+        {
+            return "";
+        }
+
+        public object CaptureState()
+        {
+            print("im capturing shit, nigga" + GetUniqueIdentifier());
+            return null;
+        }
+    }
+}
---
+++ Tattie_RPG_Take_2/Library/Collab/Original/Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SaveableEntity : MonoBehaviour
+    {
+        public string GetUniqueIdentifier()
+        {
+            return "";
+        }
+
+        public object CaptureState()
+        {
+            print("im capturing shit, nigga" + GetUniqueIdentifier());
+            return null;
+        }
+
+        public void RestoreState(object state)
+        {
+            print("s" + GetUniqueIdentifier());
+        }
+    }
+}
