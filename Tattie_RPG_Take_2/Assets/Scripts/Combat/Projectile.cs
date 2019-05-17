51eba60bc19eca70057e2312e0deb09e444f9c41
Chris <stofferdyr@gmail.com>
2019-05-17 22:39:11 +0200

    finish lecture 105. wait for next lecture to be released

--- Tattie_RPG_Take_2/Assets/Scripts/Combat/Projectile.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Combat/Projectile.cs
@@ -4,39 +4,45 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-namespace RPG.Combat
+
+public class Projectile : MonoBehaviour
 {
-    public class Projectile : MonoBehaviour
+    [SerializeField] float speed = 1;
+    [SerializeField] float capsuleHeightDivider = 2;
+    float damage = 0;
+    Health target = null;
+
+
+    void Update()
     {
-        [SerializeField] float speed = 1;
-        [SerializeField] float capsuleHeightDivider = 2;
+        if (target == null) return;
 
-        Health target = null;
-
-
-        void Update()
-        {
-            if (target == null) return;
-
-            transform.LookAt(GetAimLocation());
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-
-
-        public void SetTarget(Health target)
-        {
-            this.target = target;
-        }
-        
-        private Vector3 GetAimLocation()
-        {
-            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-            if (targetCapsule == null)
-            {
-                return target.transform.position;
-            }
-            return target.transform.position + Vector3.up * targetCapsule.height / capsuleHeightDivider;
-        }
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
+
+    public void SetTarget(Health target, float damage)
+    {
+        this.target = target;
+        this.damage = damage;
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null)
+        {
+            return target.transform.position;
+        }
+        return target.transform.position + Vector3.up * targetCapsule.height / capsuleHeightDivider;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != target) return;
+        target.TakeDamage(damage);
+        Destroy(gameObject);
+    }
 }
+
+
