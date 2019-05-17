83f968a62aab7cc41a7afd15f516b85c8bf6008a
Chris <stofferdyr@gmail.com>
2019-05-17 22:03:21 +0200

    start lecture 105

---
+++ Tattie_RPG_Take_2/Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float speed = 1;
+        [SerializeField] float capsuleHeightDivider = 2;
+
+        Health target = null;
+
+
+        void Update()
+        {
+            if (target == null) return;
+
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+
+        public void SetTarget(Health target)
+        {
+            this.target = target;
+        }
+        
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+            if (targetCapsule == null)
+            {
+                return target.transform.position;
+            }
+            return target.transform.position + Vector3.up * targetCapsule.height / capsuleHeightDivider;
+        }
+    }
+
+}
