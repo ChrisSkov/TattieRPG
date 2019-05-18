acc61112a5f1a6ab021a82be7023aee759ae47af
Chris <stofferdyr@gmail.com>
2019-05-19 00:05:27 +0200

    finish lecture 107. wait for more lectures

--- Tattie_RPG_Take_2/Assets/Scripts/Combat/Weapon.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Combat/Weapon.cs
@@ -13,12 +13,16 @@ namespace RPG.Combat
         [SerializeField] bool isRightHanded = true;
         [SerializeField] Projectile projectile = null;
 
+        const string weaponName = "Weapon";
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
+
+            DestroyOldWeapon(rightHand, leftHand);
             if (equippedPrefab != null)
             {
                 Transform handTransform = GetTransform(rightHand, leftHand);
-                Instantiate(equippedPrefab, handTransform);
+                GameObject weapon = Instantiate(equippedPrefab, handTransform);
+                weapon.name = weaponName;
             }
             if (animatorOverride != null)
             {
@@ -26,10 +30,22 @@ namespace RPG.Combat
             }
         }
 
+        private void DestroyOldWeapon(Transform rightHand, Transform leftHand)
+        {
+            Transform oldWeapon = rightHand.Find(weaponName);
+            if (oldWeapon == null)
+            {
+                oldWeapon = leftHand.Find(weaponName);
+            }
+            if (oldWeapon == null) return;
+            oldWeapon.name = "DESTROYING";
+            Destroy(oldWeapon.gameObject);
+        }
+
         private Transform GetTransform(Transform rightHand, Transform leftHand)
         {
             Transform handTransform;
-            if (isRightHanded)handTransform = rightHand;
+            if (isRightHanded) handTransform = rightHand;
             else handTransform = leftHand;
             return handTransform;
         }
