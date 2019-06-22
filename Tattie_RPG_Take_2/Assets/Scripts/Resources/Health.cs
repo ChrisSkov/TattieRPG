79aa19c5b0cebc89a71d7826a7ab662a3cc336b4
Chris <stofferdyr@gmail.com>
2019-06-22 15:52:07 +0200

    start lecture 125

--- Tattie_RPG_Take_2/Assets/Scripts/Resources/Health.cs
+++ Tattie_RPG_Take_2/Assets/Scripts/Resources/Health.cs
@@ -17,7 +17,7 @@ namespace RPG.Resources
 
         private void Start()
         {
-            healthPoints = GetComponent<BaseStats>().GetHealth();
+            healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
         }
         public float getHealth()
         {
@@ -49,7 +49,7 @@ namespace RPG.Resources
 
         public float GetPercentage()
         {
-            return 100 * (healthPoints / GetComponent<BaseStats>().GetHealth());
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetStat(Stat.Health));
         }
 
         public void Die()
@@ -68,7 +68,7 @@ namespace RPG.Resources
             Experience experience = instigator.GetComponent<Experience>();
             if (experience == null) return;
 
-            experience.GainExperience(GetComponent<BaseStats>().GetExperienceReward());
+            experience.GainExperience(GetComponent<BaseStats>().GetStat(Stat.ExperienceReward));
         }
         public object CaptureState()
         {
